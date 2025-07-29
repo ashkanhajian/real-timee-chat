84a76a03172b031fa2faa79f512464795deca389
nell2008 <ashkan.spg@gmail.com>
2025-07-29 12:42:25 +0330

    Initial commit

---
+++ chatapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    name = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Friends(models.Model):
+    pass
+
+
