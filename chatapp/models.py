ff333600f5a16f56c2f43580e4785b14bbcc2303
nell2008 <ashkan.spg@gmail.com>
2025-08-13 12:43:01 +0330

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	chatapp/models.py
    #	chatapp/templates/main/chatapp.html
    #	chatapp/urls.py

--- chatapp/models.py
+++ chatapp/models.py
@@ -6,7 +6,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     picture = models.ImageField(upload_to='img', null=True, blank=True)
+    friends = models.ManyToManyField('Friend', related_name='friends')
 
     def __str__(self):
         return self.name
 
+class Friend(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.profile.name
